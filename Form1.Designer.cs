7eef9e09c705b6c089ca2016532f6507342bc9b3
Tetsu <Tetsu@ROO>
2026-03-15 09:25:26 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SleepTimerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbProcesses = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.numHours = new System.Windows.Forms.NumericUpDown();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.numMinutes = new System.Windows.Forms.NumericUpDown();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblAlarm = new System.Windows.Forms.Label();
+            this.timeStart = new System.Windows.Forms.DateTimePicker();
+            this.btnSetAlarm = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbProcesses
+            // 
+            this.cmbProcesses.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProcesses.FormattingEnabled = true;
+            this.cmbProcesses.Location = new System.Drawing.Point(13, 60);
+            this.cmbProcesses.Name = "cmbProcesses";
+            this.cmbProcesses.Size = new System.Drawing.Size(278, 21);
+            this.cmbProcesses.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(13, 111);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Location = new System.Drawing.Point(13, 161);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(35, 13);
+            this.lblHours.TabIndex = 4;
+            this.lblHours.Text = "Hours";
+            this.lblHours.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numHours
+            // 
+            this.numHours.Location = new System.Drawing.Point(16, 201);
+            this.numHours.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numHours.Name = "numHours";
+            this.numHours.Size = new System.Drawing.Size(120, 20);
+            this.numHours.TabIndex = 5;
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Location = new System.Drawing.Point(168, 161);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(44, 13);
+            this.lblMinutes.TabIndex = 6;
+            this.lblMinutes.Text = "Minutes";
+            // 
+            // numMinutes
+            // 
+            this.numMinutes.Location = new System.Drawing.Point(171, 200);
+            this.numMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numMinutes.Name = "numMinutes";
+            this.numMinutes.Size = new System.Drawing.Size(120, 20);
+            this.numMinutes.TabIndex = 7;
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.BackColor = System.Drawing.Color.Black;
+            this.lblCountdown.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblCountdown.Location = new System.Drawing.Point(251, 251);
+            this.lblCountdown.MaximumSize = new System.Drawing.Size(300, 100);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(232, 56);
+            this.lblCountdown.TabIndex = 8;
+            this.lblCountdown.Text = "00:00:00";
+            this.lblCountdown.Click += new System.EventHandler(this.lblCountdown_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 279);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start Timer";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(106, 279);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(10, 333);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(38, 13);
+            this.lblStatus.TabIndex = 11;
+            this.lblStatus.Text = "Ready";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // lblAlarm
+            // 
+            this.lblAlarm.AutoSize = true;
+            this.lblAlarm.Location = new System.Drawing.Point(13, 398);
+            this.lblAlarm.Name = "lblAlarm";
+            this.lblAlarm.Size = new System.Drawing.Size(68, 13);
+            this.lblAlarm.TabIndex = 12;
+            this.lblAlarm.Text = "Start SDR At";
+            // 
+            // timeStart
+            // 
+            this.timeStart.CustomFormat = "MM:DD:hh:mm";
+            this.timeStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timeStart.Location = new System.Drawing.Point(13, 459);
+            this.timeStart.Name = "timeStart";
+            this.timeStart.Size = new System.Drawing.Size(200, 20);
+            this.timeStart.TabIndex = 13;
+            this.timeStart.Value = new System.DateTime(2026, 3, 15, 0, 0, 0, 0);
+            // 
+            // btnSetAlarm
+            // 
+            this.btnSetAlarm.Location = new System.Drawing.Point(13, 505);
+            this.btnSetAlarm.Name = "btnSetAlarm";
+            this.btnSetAlarm.Size = new System.Drawing.Size(112, 23);
+            this.btnSetAlarm.TabIndex = 14;
+            this.btnSetAlarm.Text = "Set Start Time";
+            this.btnSetAlarm.UseVisualStyleBackColor = true;
+            this.btnSetAlarm.Click += new System.EventHandler(this.btnSetAlarm_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.btnSetAlarm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 582);
+            this.Controls.Add(this.btnSetAlarm);
+            this.Controls.Add(this.timeStart);
+            this.Controls.Add(this.lblAlarm);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.numMinutes);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.numHours);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbProcesses);
+            this.Name = "Form1";
+            this.Text = "Program Sleep Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cmbProcesses;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.NumericUpDown numHours;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.NumericUpDown numMinutes;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblAlarm;
+        private System.Windows.Forms.DateTimePicker timeStart;
+        private System.Windows.Forms.Button btnSetAlarm;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+
+
+namespace SleepTimerApp
+{
+    public partial class Form1 : Form
+    {
+        private DateTime startTime;
+        private DateTime startTimer2;
+        private bool startTimerActive = false;
+        private int remainingSeconds = 0;
+        private int remainingSecondsTimer2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            timer2.Interval = 1000;
+            timer2.Tick += timer2_Tick;
+            LoadProcesses();
+        }
+        [DllImport("user32.dll")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        private void LoadProcesses()
+        {
+            cmbProcesses.Items.Clear();
+
+            var processes = Process.GetProcesses()
+                .Where(p => p.MainWindowHandle != IntPtr.Zero && IsWindowVisible(p.MainWindowHandle))
+                .Select(p => p.ProcessName)
+                .Distinct()
+                .OrderBy(p => p.ToLower().Contains("sdr") ? 0 : 1)
+                .ThenBy(p => p);
+
+
+            foreach (var p in processes)
+            {
+                cmbProcesses.Items.Add(p);
+            }
+
+            if (cmbProcesses.Items.Count > 0)
+                cmbProcesses.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            LoadProcesses();
+            lblStatus.Text = "Process list loaded.";
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadProcesses();
+            lblStatus.Text = "Process list refreshed.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            lblCountdown.Text = "00:00:00";
+            lblStatus.Text = "Timer canceled.";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (cmbProcesses.SelectedItem == null)
+            {
+                MessageBox.Show("Select a program first.");
+                return;
+            }
+
+            int hours = (int)numHours.Value;
+            int minutes = (int)numMinutes.Value;
+
+            remainingSeconds = (hours * 3600) + (minutes * 60);
+
+            if (remainingSeconds <= 0)
+            {
+                MessageBox.Show("Set a time first.");
+                return;
+            }
+
+            timer1.Start();
+            lblStatus.Text = "Timer running...";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            lblCountdown.Text = "00:00:00";
+            lblStatus.Text = "Timer canceled.";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*// launch SDR if alarm reached
+            if (startTimerActive && DateTime.Now >= startTime)
+            {
+                startTimerActive = false;
+                LaunchSDR();
+            }
+            */
+            remainingSeconds--;
+            
+            TimeSpan time = TimeSpan.FromSeconds(remainingSeconds);
+            lblCountdown.Text = time.ToString(@"hh\:mm\:ss");
+
+            if (remainingSeconds <= 0)
+            {
+                timer1.Stop();
+                CloseSelectedProcess();
+            }
+        }
+        ///Start APP
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // launch SDR if alarm reached
+            if (startTimerActive && DateTime.Now >= startTimer2)
+            {
+                startTimerActive = false;
+                LaunchSDR();
+            }
+            remainingSecondsTimer2--;
+        }
+        private void LaunchSDR()
+        {
+            try
+            {
+                Process.Start(@"C:\Program Files\SDR-Radio.com (V3)\SDR Console.exe");
+                lblStatus.Text = "SDR Console started.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CloseSelectedProcess()
+        {
+            try
+            {
+                string processName = cmbProcesses.SelectedItem.ToString();
+
+                var processes = Process.GetProcessesByName(processName);
+
+                foreach (var p in processes)
+                {
+                    p.Kill();
+                }
+
+                lblStatus.Text = "Program closed. Sleep timer finished.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lblCountdown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnSetAlarm_Click(object sender, EventArgs e)
+        {
+            startTimer2 = DateTime.Today + timeStart.Value.TimeOfDay;
+
+            if (startTimer2 < DateTime.Now)
+                startTimer2 = startTime.AddDays(1);
+
+            startTimerActive = true;
+
+            timer2.Start();
+
+            lblStatus.Text = "SDR start scheduled."+ startTimer2.ToString("hh:mm tt");
+        }
+    }
+}
